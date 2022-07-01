1ebe2dc84e590e80ed0c8b7b72976947d4ab13fb
Willy Fernando Nascimento <87589876+will27nasc@users.noreply.github.com>
2022-06-30 22:51:09 -0300

    Deletando parte do código
    Deletei o res.send, pois tinha adicionado o res.sendFile

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
-app.get('/', (req, res) => res.send('<h1>Bem vindo</h1>'))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
