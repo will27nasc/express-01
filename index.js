2f4c2c9d2a67d4f8e0addc25f42b4b77f0cbd21c
will27nasc <willy27nascimento@gmail.com>
2022-06-30 22:12:14 -0300

    feat: Enviando o html na resposta do servidor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>Bem vindo a homepage</h1>'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
