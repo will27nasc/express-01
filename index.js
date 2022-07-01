821f049473c95f47f6494d7c8a7527e052e535d9
will27nasc <willy27nascimento@gmail.com>
2022-06-30 22:42:21 -0300

    Atualizando-2

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-
-app.get('/', (req, res) => res.send('<h1>Bem vindo</h1>'))
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
